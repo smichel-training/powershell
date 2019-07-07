45599a478aef8420f1beac1dee0b5a95e716cb78
Sébastien MICHEL <smichel@VIAREPORT.local>
2019-07-07 07:43:13 +0200

    New script Added

---
+++ AuditPSHelp.ps1
@@ -0,0 +1,32 @@
+# Get-UpdateHelpVersion.ps1
+Param(
+    [parameter(Mandatory=$False)]
+    [String[]]
+    $Module
+)
+$HelpInfoNamespace = @{helpInfo='http://schemas.microsoft.com/powershell/help/2010/05'}
+
+if ($Module) { $Modules = Get-Module $Module -ListAvailable | where {$_.HelpInfoUri} }
+else { $Modules = Get-Module -ListAvailable | where {$_.HelpInfoUri} }
+
+foreach ($mModule in $Modules)
+{
+    Write-Host "**** NEW MODULE FETCH ****"
+    $mDir = $mModule.ModuleBase
+    Write-Host "$mDir"
+
+    if (Test-Path $mdir\*helpinfo.xml)
+    {
+        $mName=$mModule.Name
+        Write-Host "$mName"
+        $mNodes = dir $mdir\*helpinfo.xml -ErrorAction SilentlyContinue |
+            Select-Xml -Namespace $HelpInfoNamespace -XPath "//helpInfo:UICulture"
+        foreach ($mNode in $mNodes)
+        {
+            $mCulture=$mNode.Node.UICultureName
+            $mVer=$mNode.Node.UICultureVersion
+
+            [PSCustomObject]@{"ModuleName"=$mName; "Culture"=$mCulture; "Version"=$mVer}
+        }
+    }
+}
